4384c8ba0f35ad028357acd6253e3f47ec9d0e60
Joel Shadbolt <joel.hutson@mantelgroup.com.au>
2024-08-14 13:03:45 +1000

    update syntax

--- s3.tf
+++ s3.tf
@@ -3,13 +3,13 @@ resource "random_id" "random" {
 }
 
 resource "aws_s3_bucket" "s3" {
-  count = var.workspaces_pooled_settings.app_settings_persistence ? 1 : 0
+  count = var.workspaces_pooled_settings.app_settings_persistence == true ? 1 : 0
 
   bucket = "workspaces-pooled-application-persistence-${random_id.random.hex}"
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
-  count = var.workspaces_pooled_settings.app_settings_persistence ? 1 : 0
+  count = var.workspaces_pooled_settings.app_settings_persistence == true ? 1 : 0
 
   bucket = aws_s3_bucket.s3[0].id
 
