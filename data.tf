3955bccadfcc8900084e1fe533d1a11f8b629f42
Joel Shadbolt <joel.hutson@mantelgroup.com.au>
2024-08-14 12:11:42 +1000

    initial

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 data "aws_cloudformation_stack" "workspaces_pooled" {
-  name = "workspaces-pooled"
+  name = var.workspaces_pooled_settings.pool_name
 
   depends_on = [aws_cloudformation_stack.workspaces_pooled]
 }
